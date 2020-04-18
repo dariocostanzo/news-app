ff4af420db1b9c09e24b2e243d992eb7234c3d4f
dariocostanzo <darioc@xara.com>
2020-04-18 12:26:03 +0100

    removes unused components and fix styles using flexbox and removes file article.js

--- App.js
+++ App.js
@@ -1,34 +1,30 @@
-import "react-native-gesture-handler";
 import React, { useEffect, useState } from "react";
 import { StyleSheet, FlatList, Text, View, Image, Button } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 
 const styles = StyleSheet.create({
   screen: {
     padding: 20,
   },
-  imgContainer: {
+  box: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
   },
-  input: {
-    width: "100%",
-    borderColor: "black",
-    borderWidth: 1,
-    padding: 10,
-    fontSize: 18,
-  },
   listItem: {
-    padding: 10,
-    backgroundColor: "#add8e6",
+    padding: 5,
     marginVertical: 10,
   },
   thumbnail: {
-    padding: 50,
+    flex: 1,
+    padding: 40,
     width: 40,
     height: 40,
+  },
+  title: {
+    fontWeight: "bold",
+  },
+  text: {
+    flex: 1,
   },
 });
 
@@ -38,41 +34,6 @@ export default App = () => {
     "https://newsapi.org/v2/top-headlines?category=Entertainment&apiKey=";
   const API_KEY = "a984065f40434476bbdf54c45eebba7c";
 
-  function HomeScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => navigation.navigate("Details")}
-        />
-      </View>
-    );
-  }
-
-  function DetailsScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => navigation.push("Details")}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => navigation.navigate("Home")}
-        />
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-        <Button
-          title="Go back to first screen in stack"
-          onPress={() => navigation.popToTop()}
-        />
-      </View>
-    );
-  }
-
-  const Stack = createStackNavigator();
-
   useEffect(() => {
     fetch(API_URL + API_KEY)
       .then((response) => response.json())
@@ -80,32 +41,28 @@ export default App = () => {
   });
 
   return (
-    <NavigationContainer>
-      {/* <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator> */}
-      <View style={styles.screen}>
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <View style={styles.screen}>
-              <View style={styles.inputContainer}>
-                <Image
-                  style={styles.thumbnail}
-                  source={{
-                    uri: item.urlToImage,
-                  }}
-                />
-              </View>
-              <View style={styles.listItem}>
-                <Text> {"Title: " + item.title} </Text>
-                <Text> {"Data: " + item.publishedAt}</Text>
-              </View>
+    <View style={styles.screen}>
+      <FlatList
+        data={data}
+        keyExtractor={({ id }, index) => id}
+        renderItem={({ item }) => (
+          <View style={styles.box}>
+            <View style={styles.listItem}>
+              <Image
+                style={styles.thumbnail}
+                source={{
+                  uri: item.urlToImage,
+                }}
+              />
             </View>
-          )}
-        />
-      </View>
-    </NavigationContainer>
+
+            <View style={styles.text}>
+              <Text style={styles.title}> {item.title} </Text>
+              <Text> {"Date: " + item.publishedAt}</Text>
+            </View>
+          </View>
+        )}
+      />
+    </View>
   );
 };
